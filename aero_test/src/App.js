c4be9e4e04ce14b342c31a317e0ffa49acce1261
egorcko <egorcko@yandex.ru>
2019-10-31 21:34:30 +0300

    init commit

---
+++ aero_test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@/assets/styles/base.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>test</h2>
+    </div>
+  );
+}
+
+export default App;
